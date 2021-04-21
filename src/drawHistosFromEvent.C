c69605de87166e94dcc75432978ab1eddb1e9b78
Stephen Joseph Dolan <sdolan@lxplus730.cern.ch>
2021-04-21 14:48:16 +0200

    Updates for 210421 meeting, including updated less stringent cosmic criteria

--- src/drawHistosFromEvent.C
+++ src/drawHistosFromEvent.C
@@ -1,4 +1,4 @@
-R__LOAD_LIBRARY(fromUmut/GluedCubes/Analysis/TreeManager_C.so);
+R__LOAD_LIBRARY(TreeManager_C.so);
 
 #include <iomanip>
 #include <iostream>
@@ -25,7 +25,9 @@ R__LOAD_LIBRARY(fromUmut/GluedCubes/Analysis/TreeManager_C.so);
 #include <TLeaf.h>
 #include <TMath.h>
 
-TreeManager filereader("./fromUmut/GluedCubes/SpecialRun_GluedCubesNoTeflon_15April2021.root");
+//TreeManager filereader("/eos/home-s/sdolan/cubesWork/analysis/fromUmut/GluedCubes/SpecialRun_GluedCubesNoTeflon_15April2021.root");
+//TreeManager filereader("/eos/home-s/sdolan/cubesWork/analysis/fromUmut/GluedCubes/SpecialRun_GluedCubesNoTeflon_14April2021.root");
+TreeManager filereader("/eos/user/d/dsgalabe/3DprintScint/RD/Characterization_April_2021/Data/GluedCubes/SpecialRun_GluedCubesWithTeflon_19April2021.root");
 
 using namespace std;
 
@@ -36,11 +38,13 @@ vector<TH2D*> twoFaces(Long64_t, double, double, bool);
 TH3D* threeDimWLSF(Long64_t, double, double, bool);
 TH3D* threeDimMPPC(Long64_t, double, double, bool);
 TH1D* totalLightYield();
-TVector3 getMMPCPos(int);
+TVector3 getMPPCPos(int);
 void showAll(Long64_t, double, double);
+bool passCosmicSelection(Long64_t, UInt_t, bool);
 
 
-void showHighLYEvent(double lyLim, int seed=0){
+
+void showHighLYEvent(UInt_t lyLim, int seed=0){
     //Initalise RNG generator
     TRandom3* rando = new TRandom3();
     rando->SetSeed(seed);
@@ -48,7 +52,7 @@ void showHighLYEvent(double lyLim, int seed=0){
     Long64_t entries = data->GetInputTree()->GetEntries();
     int randEvNum = 0;
 
-    double totalLY = 0;
+    UInt_t totalLY = 0;
     while(true){
         randEvNum = rando->Rndm()*entries;
         data->GetMppc(randEvNum);
@@ -60,7 +64,61 @@ void showHighLYEvent(double lyLim, int seed=0){
     std::cout << "Chosen event number: " << randEvNum << std::endl;
     std::cout << "  Light Yield (ADC): " << totalLY << std::endl;
 
-    showAll(randEvNum, 1, 500);
+    showAll(randEvNum, 1, 1000);
+}
+
+void showHighADCCoincidenceEvent(UInt_t lyLim=1000, UInt_t hitLim=3, int seed=0){
+    //Initalise RNG generator
+    TRandom3* rando = new TRandom3();
+    rando->SetSeed(seed);
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+    int randEvNum = 0;
+
+    UInt_t totalLY = 0;
+    UInt_t nHits = 0;
+    while(true){
+        randEvNum = rando->Rndm()*entries;
+        data->GetMppc(randEvNum);
+        totalLY = 0;
+        nHits = 0;
+        for (int i=0;i<18;i++){
+            totalLY += data->ADC(i);
+            if(data->ADC(i) > lyLim) nHits+=1;
+        }
+        if(nHits>hitLim) break;
+    }
+
+    std::cout << "Chosen event number: " << randEvNum << std::endl;
+    std::cout << "  Light Yield (ADC): " << totalLY << std::endl;
+    std::cout << "     Number of hits: " << nHits << std::endl;
+
+    showAll(randEvNum, 1, 1000);
+}
+
+void showCosmicCoincidenceEvent(UInt_t lyLim=1000, int seed=0){
+    //Initalise RNG generator
+    TRandom3* rando = new TRandom3();
+    rando->SetSeed(seed);
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+    Long64_t randEvNum = 0;
+
+    UInt_t totalLY = 0;
+    UInt_t nHits = 0;
+    UInt_t nHitsTopX = 0;
+    UInt_t nHitsTopY = 0;
+    UInt_t nHitsBotX = 0;
+    UInt_t nHitsBotY = 0;
+    bool isHit = false;
+    vector<UInt_t> hitMPPC; 
+    vector<UInt_t> hitADC; 
+
+    while(true){
+        randEvNum = rando->Rndm()*entries;
+        if( passCosmicSelection(randEvNum, lyLim, true) ) break;
+    }
+    showAll(randEvNum, 1, 1000);
 }
 
 
@@ -70,7 +128,8 @@ void showAll(Long64_t evNum, double limLow=1, double limHigh=300){
     TH3D* histoWLSF = threeDimWLSF(evNum, limLow, limHigh, true);
     TH3D* histoMPPC = threeDimMPPC(evNum, limLow, limHigh, true);
 
-    TCanvas *c1 = new TCanvas("c1","multipads",950,950);
+    gStyle->SetOptTitle(1); 
+    TCanvas *c1 = new TCanvas("c1","multipads",1350,950);
     gStyle->SetOptStat(0);
     c1->Divide(2,2);
     c1->cd(1);
@@ -89,8 +148,8 @@ void showAll(Long64_t evNum, double limLow=1, double limHigh=300){
 vector<TH2D*> twoFaces(Long64_t evNum, double limLow=1, double limHigh=300, bool noDraw=false){
 
     //Setup hhistos
-    TH2D* facexzHisto = new TH2D("xz", "xz;X [cm]; Z [cm]", 7,0,7, 7,0,7);
-    TH2D* faceyzHisto = new TH2D("yz", "yz;Y [cm]; Z [cm]", 7,0,7, 7,0,7);
+    TH2D* facexzHisto = new TH2D("xz", "XZ View;X [cm]; Z [cm]", 7,0,7, 7,0,7);
+    TH2D* faceyzHisto = new TH2D("yz", "YZ View;Y [cm]; Z [cm]", 7,0,7, 7,0,7);
 
     facexzHisto->GetZaxis()->SetRangeUser(limLow, limHigh);
     faceyzHisto->GetZaxis()->SetRangeUser(limLow, limHigh);
@@ -102,14 +161,14 @@ vector<TH2D*> twoFaces(Long64_t evNum, double limLow=1, double limHigh=300, bool
 
     //Loop over the MPPCs in the event
     for(int i=1; i<=18; i++){
-        TVector3 pos = getMMPCPos(i);
+        TVector3 pos = getMPPCPos(i);
         //Face 1 or Face 3
         if(pos.Y()==0 || pos.Y()==7){
-            facexzHisto->Fill(pos.X(), pos.Z(), data->ADC(i));
+            facexzHisto->Fill(pos.X(), pos.Z(), (double)data->ADC(i-1));
         }
         //Face 2 or Face 4
         else if(pos.X()==0 || pos.X()==7){
-            faceyzHisto->Fill(pos.Y(), pos.Z(), data->ADC(i));
+            faceyzHisto->Fill(pos.Y(), pos.Z(), (double)data->ADC(i-1));
         }
         else{
             std::cout << "WARNING: found invalid MPPC position for MPPC " << i << std::endl;
@@ -137,7 +196,7 @@ vector<TH2D*> twoFaces(Long64_t evNum, double limLow=1, double limHigh=300, bool
 
 TH3D* threeDimWLSF(Long64_t evNum, double limLow=1, double limHigh=300, bool noDraw=false){
     //Setup hhistos
-    TH3D* histo = new TH3D("xyzWLSF", "xyzWLSF;X [mm]; Y [mm]; Z[mm]", 14,0,70, 14,0,70 , 14,0,70);
+    TH3D* histo = new TH3D("xyzWLSF", "3D WLSF View;X [mm]; Y [mm]; Z[mm]", 14,0,70, 14,0,70 , 14,0,70);
 
     Mppc *data = filereader.tmCD(); 
     Long64_t entries = data->GetInputTree()->GetEntries();
@@ -146,17 +205,17 @@ TH3D* threeDimWLSF(Long64_t evNum, double limLow=1, double limHigh=300, bool noD
 
     //Loop over the MPPCs in the event
     for(int i=1; i<=18; i++){
-        TVector3 pos = getMMPCPos(i);
+        TVector3 pos = getMPPCPos(i);
         //Face 1 or Face 3
         if(pos.Y()==0 || pos.Y()==7){
             for(int j=2; j<=12; j++){
-              histo->Fill(10*pos.X()+5, 5*j, 10*pos.Z()+5, data->ADC(i));
+              histo->Fill(10*pos.X()+5, 5*j, 10*pos.Z()+5, (double)data->ADC(i-1));
             }
         }
         //Face 2 or Face 4
         else if(pos.X()==0 || pos.X()==7){
             for(int j=2; j<=12; j++){
-              histo->Fill(5*j, 10*pos.Y()+5, 10*pos.Z()+5, data->ADC(i));
+              histo->Fill(5*j, 10*pos.Y()+5, 10*pos.Z()+5, (double)data->ADC(i-1));
             }
         }
         else{
@@ -173,7 +232,7 @@ TH3D* threeDimWLSF(Long64_t evNum, double limLow=1, double limHigh=300, bool noD
 
 TH3D* threeDimMPPC(Long64_t evNum, double limLow=1, double limHigh=300, bool noDraw=false){
     //Setup hhistos
-    TH3D* histo = new TH3D("xyzMPPC", "xyzMPPC;X [cm]; Y [cm]; Z[cm]", 7,0,7, 7,0,7 , 7,0,7);
+    TH3D* histo = new TH3D("xyzMPPC", "3D MPPC View;X [cm]; Y [cm]; Z[cm]", 7,0,7, 7,0,7 , 7,0,7);
 
     Mppc *data = filereader.tmCD(); 
     Long64_t entries = data->GetInputTree()->GetEntries();
@@ -182,14 +241,14 @@ TH3D* threeDimMPPC(Long64_t evNum, double limLow=1, double limHigh=300, bool noD
 
     //Loop over the MPPCs in the event
     for(int i=1; i<=18; i++){
-        TVector3 pos = getMMPCPos(i);
+        TVector3 pos = getMPPCPos(i);
         //Face 1 or Face 3
         if(pos.Y()==0 || pos.Y()==7){
-            histo->Fill(pos.X(), 0.0, pos.Z(), data->ADC(i));
+            histo->Fill(pos.X(), 0.0, pos.Z(), (double)data->ADC(i-1));
         }
         //Face 2 or Face 4
         else if(pos.X()==0 || pos.X()==7){
-            histo->Fill(0.0, pos.Y(), pos.Z(), data->ADC(i));
+            histo->Fill(0.0, pos.Y(), pos.Z(), (double)data->ADC(i-1));
         }
         else{
             std::cout << "WARNING: found invalid MPPC position for MPPC " << i << std::endl;
@@ -212,14 +271,214 @@ TH1D* totalLightYield()
 	{
 		data->GetMppc(ientry);
         double totalLY = 0;
-		for (int i=0;i<18;i++) totalLY += data->ADC(i);
+        for (int i=0;i<18;i++) totalLY += data->ADC(i);
         lyHisto->Fill(totalLY);
 	}
     TCanvas* c1 = new TCanvas();
     lyHisto->Draw();
+
+    return lyHisto;
 }
 
-TVector3 getMMPCPos(int mppcNum){
+vector<TH1D*> perMPPCLightYield()
+{
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+
+    vector<TH1D*> listOfHist; 
+    for (int i=1;i<=18;i++){
+        TH1D* lyHisto = new TH1D(Form("lyHisto%d",i), Form("MPPC %d; Light Yield [ADC]; Counts",i), 300, 0, 6000);
+        listOfHist.push_back(lyHisto);
+    }
+
+    for( Long64_t ientry=0; ientry<entries; ++ientry )
+    {
+        data->GetMppc(ientry);
+        for (int i=0;i<18;i++){
+            listOfHist[i]->Fill(data->ADC(i));
+        }
+    }
+    gStyle->SetOptTitle(1); 
+    TCanvas *c1 = new TCanvas("c1","c1",1900,950);
+    gStyle->SetOptStat(0);
+    c1->Divide(6,3);
+    for (int i=0;i<18;i++){
+        gPad->SetLogy();
+        c1->cd(i+1);
+        listOfHist[i]->Draw();
+    }
+
+    return listOfHist;
+}
+
+vector<TH1D*> perMPPCCosmicLightYieldAndHits(UInt_t lyLim=1500)
+{
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+
+    TH1D* hitsHisto = new TH1D("hitsHisto", "hitsHisto;MPPC Number; Hits From Cosmics", 18, 1, 19);
+
+    vector<TH1D*> listOfHist; 
+    for (int i=1;i<=18;i++){
+        TH1D* lyHisto = new TH1D(Form("lyHisto%d",i), Form("MPPC %d; Light Yield [ADC]; Counts",i), 300, lyLim, 6000);
+        listOfHist.push_back(lyHisto);
+    }
+
+    for( Long64_t ientry=0; ientry<entries; ++ientry )
+    {
+        // Check if we pass the cosmic selection for this event
+        if( !passCosmicSelection(ientry, lyLim, false) ) continue;
+
+        data->GetMppc(ientry);
+        for (int i=0;i<18;i++){
+            if(data->ADC(i) > lyLim){
+                gPad->SetLogy();
+                hitsHisto->Fill(i+1);
+                listOfHist[i]->Fill(data->ADC(i));
+            }
+        }
+    }
+
+    //Setup histos
+    TH2D* facexzHisto = new TH2D("MeanCosmicLY_XZ", "Mean Cosmic LY;X [cm]; Z [cm]", 7,0,7, 7,0,7);
+    TH2D* faceyzHisto = new TH2D("MeanCosmicLY_YZ", "Mean Cosmic LY;Y [cm]; Z [cm]", 7,0,7, 7,0,7);
+
+    facexzHisto->GetZaxis()->SetRangeUser(lyLim, 3000);
+    faceyzHisto->GetZaxis()->SetRangeUser(lyLim, 3000);
+
+    //Loop over the MPPCs 
+    for(int i=1; i<=18; i++){
+        TVector3 pos = getMPPCPos(i);
+        //Face 1 or Face 3
+        if(pos.Y()==0 || pos.Y()==7){
+            facexzHisto->Fill(pos.X(), pos.Z(), listOfHist[i-1]->GetMean());
+        }
+        //Face 2 or Face 4
+        else if(pos.X()==0 || pos.X()==7){
+            faceyzHisto->Fill(pos.Y(), pos.Z(), listOfHist[i-1]->GetMean());
+        }
+        else{
+            std::cout << "WARNING: found invalid MPPC position for MPPC " << i << std::endl;
+            pos.Print();
+        }
+    }
+
+    gStyle->SetOptTitle(1); 
+    TCanvas *c1 = new TCanvas("c1","c1",1900,950);
+    gStyle->SetOptStat(0);
+    c1->Divide(6,3);
+    for (int i=0;i<18;i++){
+        c1->cd(i+1);
+        listOfHist[i]->Draw();
+    }
+    TCanvas *c2 = new TCanvas();
+    hitsHisto->Draw();
+
+    TCanvas *c3 = new TCanvas("c3","multipads",1600,700);
+    gStyle->SetOptStat(0);
+    c3->Divide(2,1);
+    c3->cd(1);
+    facexzHisto->Draw("colzTEXT45");
+    c3->cd(2);
+    faceyzHisto->Draw("colzTEXT45");
+
+    return listOfHist;
+}
+
+
+
+TH1D* nHitsHisto(UInt_t lyLim=1500)
+{
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+
+    TH1D* nHitsHisto = new TH1D("nHitsHisto", "nHitsHisto", 9, 0, 9);
+    UInt_t nHits = 0;
+    for( Long64_t ientry=0; ientry<entries; ++ientry )
+    {
+        data->GetMppc(ientry);
+        UInt_t totalLY = 0;
+        nHits = 0;
+        for (int i=0;i<18;i++){
+            totalLY += data->ADC(i);
+            if(data->ADC(i) > lyLim) nHits+=1;
+        }
+        nHitsHisto->Fill((double)nHits);
+    }
+    TCanvas* c1 = new TCanvas();
+    nHitsHisto->Draw();
+
+    return nHitsHisto;
+}
+
+TH1D* coincidenceRateHisto(UInt_t lyLim=1500)
+{
+    Mppc *data = filereader.tmCD(); 
+    Long64_t entries = data->GetInputTree()->GetEntries();
+
+    TH1D* isCoincidence = new TH1D("isCoincidence", "isCoincidence", 2, 0, 2);
+
+    for( Long64_t ientry=0; ientry<entries; ++ientry )
+    {
+        if(passCosmicSelection(ientry, lyLim, false)) isCoincidence->Fill(1.5);
+        else isCoincidence->Fill(0.5);
+    }
+    TCanvas* c1 = new TCanvas();
+    isCoincidence->Draw();
+    isCoincidence->Print("all");
+
+    return isCoincidence;
+}
+
+
+bool passCosmicSelection(Long64_t evNum, UInt_t lyLim, bool verbose){
+    bool pass = false;
+
+    Mppc *data = filereader.tmCD(); 
+    data->GetMppc(evNum);
+
+    UInt_t totalLY = 0;
+    UInt_t nHits = 0;
+    UInt_t nHitsTopX = 0;
+    UInt_t nHitsTopY = 0;
+    UInt_t nHitsBotX = 0;
+    UInt_t nHitsBotY = 0;
+    bool isHit = false;
+    vector<UInt_t> hitMPPC; 
+    vector<UInt_t> hitADC; 
+
+    for (Int_t i=0;i<18;i++){
+        totalLY += data->ADC(i);
+        if(data->ADC(i) > lyLim) isHit=true;
+        else isHit = false;
+        Int_t mppcNum = i+1;
+        if(isHit){
+            if( mppcNum == 4 || mppcNum == 14 || mppcNum == 13) nHitsTopX+=1;
+            if( mppcNum == 9 || mppcNum == 8  || mppcNum == 18) nHitsTopY+=1;
+            if( mppcNum == 1 || mppcNum == 11 || mppcNum == 10) nHitsBotX+=1;
+            if( mppcNum == 6 || mppcNum == 5  || mppcNum == 15) nHitsBotY+=1;
+            nHits++;
+            hitMPPC.push_back(mppcNum);
+            hitADC.push_back(data->ADC(i));
+        }
+    }
+    //if(nHits>=6 && nHitsTopX==1 && nHitsTopY==1 && nHitsBotX==1 && nHitsBotY==1) pass = true;
+    if(nHitsTopX==1 && nHitsTopY==1 && nHitsBotX==1 && nHitsBotY==1) pass = true;
+    else pass = false;
+
+    if(pass == true && verbose){
+        std::cout << "Chosen event number: " << evNum << std::endl;
+        std::cout << "  Light Yield (ADC): " << totalLY << std::endl;
+        std::cout << "     Number of hits: " << nHits << std::endl;
+        for (int i=0; i<hitMPPC.size(); i++){
+          std::cout << "Hit MPPC: " << hitMPPC[i] << ", ADC counts: " << hitADC[i] << std::endl;
+        }
+    }
+
+    return pass;
+}
+
+TVector3 getMPPCPos(int mppcNum){
     TVector3 pos;
     double offset = 0.5; 
 
